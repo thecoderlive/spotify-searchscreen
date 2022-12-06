eaaf16cd515e0bd81c20f55c351491b097b57e7d
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-06 18:46:31 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,28 @@
+export const item = {
+    "id": "d5be3f1e-c972-4149-b558-e8aeb265012c",
+    "trending_event_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png",
+    "trending_event_title": "For history food ago southern yourself person itself. Government tough job politics little fast I.",
+    "about_trending_event": "So student young guy news. Letter ask as statement special situation. Street past wide.",
+    "search_results": [
+        {
+            "id": "badff018-e65a-46da-8c12-c90a2350b814",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+        },
+        {
+            "id": "9bb0cacc-b694-4cc2-b188-808b728219ac",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+        },
+        {
+            "id": "52bea514-d07a-4dc4-8542-a709feeb878f",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+        },
+        {
+            "id": "5990928f-e915-4f90-9be4-eb83d0689a79",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+        },
+        {
+            "id": "b51d1f5a-6bc1-4d25-918b-1b1843f3f84a",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+        }
+    ]
+}
